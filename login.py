6d1c21cabd2a1979ae87657dbcf7af372f3bce62
Margaret Tai <mtai5@student.monash.edu>
2024-04-28 21:23:40 +1000

    Worked on login control function, including user input for landing page menu choice functionality

--- login.py
+++ login.py
@@ -1,3 +1,6 @@
+from user_interface import UserInterface
+
+
 class Login:
     def __init__(self):
         pass
@@ -7,3 +10,9 @@ class Login:
         customer_email = "member@student.monash.edu"
         main_choice = "Enter your choice, or 'Q' to Quit: "
         wrong_choice = "Please enter a valid menu option"
+        ui_obj = UserInterface()
+        ui_obj.display_landing_page()
+        user_choice = input(main_choice)
+        if user_choice[0] == "" or user_choice[0] == " ":
+            print(wrong_choice)
+            user_choice = input(main_choice)
