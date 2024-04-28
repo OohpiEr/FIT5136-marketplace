d1f237c24b5e1a9cd35f314aacd72653707b0109
Margaret Tai <mtai5@student.monash.edu>
2024-04-28 21:16:29 +1000

    Worked on the beginning of login functionality, login control in Login class

--- login.py
+++ login.py
@@ -1,1 +1,9 @@
 class Login:
+    def __init__(self):
+        pass
+
+    def login_control(self):
+        admin_email = "admin@merchant.monash.edu"
+        customer_email = "member@student.monash.edu"
+        main_choice = "Enter your choice, or 'Q' to Quit: "
+        wrong_choice = "Please enter a valid menu option"
