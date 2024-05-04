c13508a292e6ebf4532e85a5096207fd86494a63
Margaret Tai <mtai5@student.monash.edu>
2024-05-05 09:57:31 +1000

    Fixed over-indentation in several classes

--- domain/Admin.py
+++ domain/Admin.py
@@ -1,7 +1,5 @@
 from app import App as app
 
 class Admin:
-    def add_product(self, name, brand, description, quantity,
-                           sub_category_id, og_price, member_price):
-        app.db.add_product(name, brand, description, quantity,
-                           sub_category_id, og_price, member_price)
+    def add_product(self, name, brand, description, quantity, sub_category_id, og_price, member_price):
+        app.db.add_product(name, brand, description, quantity, sub_category_id, og_price, member_price)
