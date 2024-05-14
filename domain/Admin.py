433781806dab2cb6738493e989a9c43140dafa4d
Mingzhu Qian <mqia0006@student.monash.edu>
2024-05-14 20:46:20 +1000

    add edit product function

--- domain/Admin.py
+++ domain/Admin.py
@@ -5,3 +5,7 @@ from DbHelper import DbHelper as db
 class Admin:
     def add_product(self, name, brand, description, quantity, sub_category_id, og_price, member_price):
         db.add_product(name, brand, description, quantity, sub_category_id, og_price, member_price)
+
+    def update_product(self, product_id, name=None, brand=None, description=None, quantity=None, sub_category_id=None,
+                       og_price=None, member_price=None):
+        db.update_product(product_id, name, brand, description, quantity, sub_category_id, og_price, member_price)
