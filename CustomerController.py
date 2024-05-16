c1eb260a1d0662eda602d327ea418cc43df2bfbe
Margaret Tai <mtai5@student.monash.edu>
2024-05-16 14:38:36 +1000

    Moved add to cart function to CustomerController class

--- CustomerController.py
+++ CustomerController.py
@@ -2,6 +2,7 @@ from UserInputError import UserInputError
 from CustomerInterface import CustomerInterface
 # from RowNotFoundError import RowNotFoundError
 from domain.Customer import Customer
+import csv
 
 
 class CustomerController():
@@ -19,8 +20,17 @@ class CustomerController():
         pass
     
     def add_to_cart(self):
-        # TODO: implement add_to_cart
-        pass
+        cart_add = input("Add to Cart (please enter the product ID number of the product you wish to add to cart): ")
+        with open('db/product.txt', 'r+', newline='') as f2:
+            with open('db/cart.txt', 'a', newline='') as w2:
+                reader = csv.reader(f2, delimiter=',')
+                for row in reader:
+                    if row and row[0] == cart_add:  # Code inspired from Author: wwii, Link: https://stackoverflow.com/questions/39336449/python-check-if-value-in-csv-file
+                        w2.write(str(row))
+                        w2.write('\n')
+                        print("You have successfully added ", row)
+                    else:
+                        print("That item is not in stock or is not selling")
 
     def customer_control(self):
         quit_flag = False
